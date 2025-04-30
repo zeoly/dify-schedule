53bf0f46046459a9bdf6b82fbbdfba006abb5df5
Zack Zeng <zeoly100@163.com>
2025-04-30 17:06:11 +0800

    Update dify.js
    修改title

--- workflow/dify.js
+++ workflow/dify.js
@@ -61,7 +61,7 @@ async function run(args) {
 
     const message = messageList.join(`\n${"-".repeat(15)}\n`);
     Notify.pushMessage({
-      title: "Dify工作流定时助手",
+      title: "智子系AI项目日报",
       content: message,
       msgtype: "text"
     });
